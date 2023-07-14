2100dff90887872264cbd1c2958d24e1ed87089b
AlessioAbbati <alex_117@live.it>
2023-07-14 15:18:06 +0200

    add route in apiphp

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('projects', [ProjectController::class, 'index'])->name('api.projects.index');
