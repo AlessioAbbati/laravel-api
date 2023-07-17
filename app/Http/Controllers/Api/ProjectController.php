3d55ccd58e37a45ecceda35b94b93452d4c9c2d3
AlessioAbbati <alex_117@live.it>
2023-07-17 19:23:59 +0200

    fix show in project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -20,7 +20,7 @@ class ProjectController extends Controller
     public function show($slug)
     {
         $project = Project::where('slug', $slug)->firstOrFail();
-        return response()->json();
+        return response()->json($project);
     }
 
     
